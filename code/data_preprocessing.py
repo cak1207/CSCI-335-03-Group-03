0b10018fbf5d6271e75d53ff4f1bf24bca76f3ea
Cameron <cak1207@g.rit.edu>
2025-10-15 13:18:56 -0400

    data downloads and previews

--- code/data_preprocessing.py
+++ code/data_preprocessing.py
@@ -2,21 +2,18 @@ import kagglehub
 from kagglehub import KaggleDatasetAdapter
 import pandas as pd
 from pandas import DataFrame as df
+import os
 
+# downloads dataset to local machine
+dataset_dir = kagglehub.dataset_download("jp797498e/twitter-entity-sentiment-analysis")
 
-train_df = kagglehub.dataset_load(
-  KaggleDatasetAdapter.PANDAS,
-  "jp797498e/twitter-entity-sentiment-analysis",
-  "twitter_training.csv",
-  pandas_kwargs={"encoding": "ISO-8859-1"}
-)
+train_path = os.path.join(dataset_dir, "twitter_training.csv")
+valid_path = os.path.join(dataset_dir, "twitter_validation.csv")
 
-valid_df = kagglehub.dataset_load(
-  KaggleDatasetAdapter.PANDAS,
-  "jp797498e/twitter-entity-sentiment-analysis",
-  "twitter_validation.csv",
-  pandas_kwargs={"encoding": "ISO-8859-1"}
-)
+train_df = pd.read_csv(train_path, encoding="utf-8", engine="python", on_bad_lines="skip")
+train_df.columns = ["tweet_id", "entity", "sentiment", "tweet_content"]
+valid_df = pd.read_csv(valid_path, encoding="utf-8", engine="python", on_bad_lines="skip")
+valid_df.columns = ["tweet_id", "entity", "sentiment", "tweet_content"]
 
-print("First 5 training records:\n", train_df.head())
-print("\nFirst 5 validation records:\n", valid_df.head())
+print("Training set preview:\n", train_df.head())
+print("\nValidation set preview:\n", valid_df.head())
