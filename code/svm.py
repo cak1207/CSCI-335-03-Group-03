5c66f2f6e4fd839d43778682497e92301a657aa8
Edison <edisonpan247@gmail.com>
2025-11-27 21:30:02 -0500

    increased to 15000 samples

--- code/svm.py
+++ code/svm.py
@@ -1,9 +1,11 @@
 from data_preprocessing import load_and_clean_data, encode_labels, vectorize_text
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,accuracy_score,f1_score, precision_score
+from sklearn.metrics import classification_report,accuracy_score,f1_score, precision_score, recall_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 import numpy as np
+import pickle
+
 df = load_and_clean_data()
 
 y, encoder = encode_labels(df)
@@ -21,7 +23,7 @@ svc = SVC(max_iter=100000)
 
 np.random.seed(42)
 
-range = np.random.choice(x_train_tfidf.shape[0], size=12000, replace=False)
+range = np.random.choice(x_train_tfidf.shape[0], size=15000, replace=False)
 x_train_small = x_train_tfidf[range]
 y_train_small = y_train[range]
 
@@ -67,14 +69,29 @@ grid.fit(x_train_small, y_train_small)
 
 y_pred = grid.predict(x_test_tfidf)
 
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
+
 print("\nBest Parameters:")
 print(grid.best_params_)
 
 print("\nBest CV Score:", grid.best_score_)
-print("Best Test Accuracy:", accuracy_score(y_test, y_pred))
 
 weighted_f1 = f1_score(y_test, y_pred, average='weighted')
 print("Best Test Weighted F1:", weighted_f1)
 
+print("Best Test Accuracy:", accuracy_score(y_test, y_pred))
+
+print("\nPrecision (weighted):", precision)
+
+print("Recall (weighted):", recall)
+
+
+
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred, target_names=encoder.classes_))
+
+best_svc = grid.best_estimator_
+
+with open("best_svc_model.pkl", "wb") as f:
+    pickle.dump(best_svc, f)
