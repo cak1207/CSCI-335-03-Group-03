7e386de0a54b58e09c30ef34ca3ba99f7b87f020
Edison <edisonpan247@gmail.com>
2025-12-08 00:00:58 -0500

    got rid of extra function

--- code/data_preprocessing.py
+++ code/data_preprocessing.py
@@ -5,26 +5,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def load_and_clean_data():
-    # downloads dataset to local machine
-    dataset_dir = kagglehub.dataset_download("jp797498e/twitter-entity-sentiment-analysis")
-
-    train_path = os.path.join(dataset_dir, "twitter_training.csv")
-    valid_path = os.path.join(dataset_dir, "twitter_validation.csv")
-
-    train_df = pd.read_csv(train_path, encoding="utf-8", engine="python", on_bad_lines="skip")
-    train_df.columns = ["tweet_id", "entity", "sentiment", "tweet_content"]
-
-    # Drop missing text entries
-    train_df = train_df.dropna(subset=['tweet_content'])
-
-
-    valid_df = pd.read_csv(valid_path, encoding="utf-8", engine="python", on_bad_lines="skip")
-    valid_df.columns = ["tweet_id", "entity", "sentiment", "tweet_content"]
-
-    return train_df, valid_df
-
-
 def load_and_clean_data(train_path=None, valid_path=None):
 
     # If no paths provided, use default data
